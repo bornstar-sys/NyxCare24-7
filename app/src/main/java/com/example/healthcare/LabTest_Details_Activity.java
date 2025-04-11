f6e9ce5654e0acc4d723dbf45a9a94fa7e6f67a4
Roshan kumar <bornstar440@gmail.com>
2025-04-12 00:17:40 +0530

    Initial commit with LabTest UI

--- app/src/main/java/com/example/healthcare/LabTest_Details_Activity.java
+++ app/src/main/java/com/example/healthcare/LabTest_Details_Activity.java
@@ -1,11 +1,14 @@
 package com.example.healthcare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,13 +45,25 @@ public class LabTest_Details_Activity extends AppCompatActivity {
         package_name.setText(intent.getStringExtra("text1"));
         ed_details.setText(intent.getStringExtra("text2"));
         total_cost.setText("Total Cost : "+intent.getStringExtra("text3")+"/-");
-        /*
+
         btnAddToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LabTest_Details_Activity.this,));
+                SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                String username = sharedPreferences.getString("username","").toString();
+                String product = package_name.getText().toString();
+                float price = Float.parseFloat(intent.getStringExtra("text3").toString());
+
+                DataBase db = new DataBase(getApplicationContext(),"healthcare",null,1);
+                if (db.checkCart(username,product)==1){
+                    Toast.makeText(getApplicationContext(),"Product already Added",Toast.LENGTH_SHORT).show();
+                }else{
+                    db.addCart(username,product,price,"lab");
+                    Toast.makeText(getApplicationContext(),"Record Inserted to Cart",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LabTest_Details_Activity.this,LabTest_Activity.class));
+                }
             }
-        });*/
+        });
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
