85bc504b252b154ea9ad4974703548d947fd8a73
Roshan kumar <bornstar440@gmail.com>
2025-04-13 13:16:19 +0530

    Changes restored to previous format

--- app/src/main/java/com/example/healthcare/LabTest_Activity.java
+++ app/src/main/java/com/example/healthcare/LabTest_Activity.java
@@ -3,6 +3,7 @@ package com.example.healthcare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -90,6 +91,17 @@ public class LabTest_Activity extends AppCompatActivity {
                 new String[]{"Line1","Line2","Line3","Line4","Line5"},
                 new int[]{R.id.line_a,R.id.line_b,R.id.line_c,R.id.line_d,R.id.line_e});
         listview.setAdapter(sa);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it  = new Intent(LabTest_Activity.this,LabTest_Details_Activity.class);
+                it.putExtra("text1",packages[i][0]);
+                it.putExtra("text2",packages[i]);
+                it.putExtra("text3",packages[i][4]);
+                startActivity(it);
+            }
+        });
 
     }
 }
